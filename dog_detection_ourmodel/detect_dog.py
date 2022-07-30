5c03d0cafeb477b3028837a3bc223f7d3affb59b
Heather Pieszala <heather.pieszala@berkeley.edu>
2022-07-30 19:54:15 -0400

    trying model with pth directly

--- dog_detection_ourmodel/detect_dog.py
+++ dog_detection_ourmodel/detect_dog.py
@@ -16,12 +16,34 @@ import time
 import sys
 import io
 
+import torch
+from torch.backends import cudnn
+
+from backbone import EfficientDetBackbone
+import matplotlib.pyplot as plt
+import numpy as np
+
+from efficientdet.utils import BBoxTransform, ClipBoxes
+from utils.utils import preprocess, invert_affine, postprocess
+
 MQTT_HOST="mosquitto-service"
 MQTT_PORT=1883
 MQTT_TOPIC = "dogdetect/images"
 
 
-doggies = cv.CascadeClassifier('efficientdet-d0_74_17600.xml')
+#direct efficientdet
+compound_coef = 0
+force_input_size = None  # set None to use default size
+
+threshold = 0.2
+iou_threshold = 0.2
+
+use_cuda = True
+use_float16 = False
+cudnn.fastest = True
+cudnn.benchmark = True
+
+obj_list = [ 'other', 'sit' ]
 
 cap = cv.VideoCapture(0)
 
@@ -75,17 +97,68 @@ time.sleep(1)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    img_path = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
-    image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    #dogs = doggies.detectMultiScale(image, (1.1, 3))
+    # tf bilinear interpolation is different from any other's, just make do
+    # tf bilinear interpolation is different from any other's, just make do
+    input_sizes = [512, 640, 768, 896, 1024, 1280, 1280, 1536]
+    input_size = input_sizes[compound_coef] if force_input_size is None else force_input_size
+    ori_imgs, framed_imgs, framed_metas = preprocess_video(img_path, max_size=input_size)
 
-    # Don't do anything if there's 
-    # no dog
-    #amount_found = len(dog)
-	  
-    #if amount_found != 0:
+    if use_cuda:
+        x = torch.stack([torch.from_numpy(fi).cuda() for fi in framed_imgs], 0)
+    else:
+        x = torch.stack([torch.from_numpy(fi) for fi in framed_imgs], 0)
+
+    x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
+
+    model = EfficientDetBackbone(compound_coef=compound_coef, num_classes=len(obj_list),
+
+                             # replace this part with your project's anchor config
+                             ratios=[(1.0, 1.0), (1.3, 0.8), (1.9, 0.5)],
+                             scales=[2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)])
+
+    model.load_state_dict(torch.load('efficientdet-d0_74_17600.pth'))
+    model.requires_grad_(False)
+    model.eval()
+
+    if use_cuda:
+        model = model.cuda()
+    if use_float16:
+        model = model.half()
+
+    with torch.no_grad():
+        features, regression, classification, anchors = model(x)
+    
+        regressBoxes = BBoxTransform()
+        clipBoxes = ClipBoxes()
+
+        out = postprocess(x,
+                      anchors, regression, classification,
+                      regressBoxes, clipBoxes,
+                      threshold, iou_threshold)
+
+    out = invert_affine(framed_metas, out)
+
+    for i in range(len(ori_imgs)):
+        if len(out[i]['rois']) == 0:
+            continue
+        ori_imgs[i] = ori_imgs[i].copy()
+        for j in range(len(out[i]['rois'])):
+            (x1, y1, x2, y2) = out[i]['rois'][j].astype(np.int)
+            cv.rectangle(ori_imgs[i], (x1, y1), (x2, y2), (255, 255, 0), 2)
+            obj = obj_list[out[i]['class_ids'][j]]
+            score = float(out[i]['scores'][j])
+            print(obj)
+            print('SCORE',score)
+
+            cv.putText(ori_imgs[i], '{}, {:.3f}'.format(obj, score),
+                    (x1, y1 + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (255, 255, 0), 1)
+
+            plt.imshow(ori_imgs[i])
       
-    #cv.imshow('frame',image)
+    cv.imshow('frame',image)
     #for (x,y,w,h) in dogs:
         #cv.rectangle(image,(x,y),(x+w,y+h),(416,0,0),2)
         #dog = image[y:y+h, x:x+w]
